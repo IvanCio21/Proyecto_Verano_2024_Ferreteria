7f7fe2d96ed9c0f4f6d254bef6c1b07e6f83dac5
IvanCio21 <ivan.espinoza.mora@est.una.ac.cr>
2024-12-24 20:49:48 -0600

    Intentando la vista idkgf algo estoy haciendo

--- src/Vista/View.java
+++ src/Vista/View.java
@@ -4,7 +4,6 @@ import javax.swing.*;
 
 public class View {
     private JPanel panel1;
-    private JTextArea categoriaTextArea;
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
