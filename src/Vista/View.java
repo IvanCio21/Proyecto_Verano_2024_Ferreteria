f37298bf7b6be54a14558b952de13d19614ba966
IvanCio21 <ivan.espinoza.mora@est.una.ac.cr>
2024-12-24 20:30:58 -0600

    Intentando la vista idk

---
+++ src/Vista/View.java
@@ -0,0 +1,13 @@
+package Vista;
+
+import javax.swing.*;
+
+public class View {
+    private JPanel panel1;
+    private JTextArea categoriaTextArea;
+    private JTextField textField1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
