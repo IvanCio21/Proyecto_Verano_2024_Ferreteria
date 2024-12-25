ca99e1614e87e072e61a3b6990b13ea275efa345
IvanCio21 <ivan.espinoza.mora@est.una.ac.cr>
2024-12-24 21:49:37 -0600

    Intentando la vista idkgf algo estoy haciendo

--- src/Vista/View.java
+++ src/Vista/View.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 
 public class View {
 
+    private JTextField textField1;
+
     private void createUIComponents() {
         // TODO: place custom component creation code here
     }
