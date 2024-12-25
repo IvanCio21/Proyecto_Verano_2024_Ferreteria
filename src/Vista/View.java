8da241a9ceae65e922da08c099f9449c5199f76b
IvanCio21 <ivan.espinoza.mora@est.una.ac.cr>
2024-12-24 20:36:51 -0600

    Intentando la vista idkgf

--- src/Vista/View.java
+++ src/Vista/View.java
@@ -5,7 +5,6 @@ import javax.swing.*;
 public class View {
     private JPanel panel1;
     private JTextArea categoriaTextArea;
-    private JTextField textField1;
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
