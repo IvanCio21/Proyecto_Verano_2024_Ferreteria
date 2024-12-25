d8b709b3b982bdb8d54f8a961f9875ed4e7a55ba
IvanCio21 <ivan.espinoza.mora@est.una.ac.cr>
2024-12-24 20:53:44 -0600

    Intentando la vista idkgf algo estoy haciendo

--- src/Vista/View.java
+++ src/Vista/View.java
@@ -3,7 +3,6 @@ package Vista;
 import javax.swing.*;
 
 public class View {
-    private JPanel panel1;
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
