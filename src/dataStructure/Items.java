b40c520dab983d9ebc3c337d4601658c129cb827
waffleexe <sharonamador169@gmail.com>
2024-12-26 21:50:03 -0600

    Objetos

--- src/dataStructure/Items.java
+++ src/dataStructure/Items.java
@@ -1,7 +1,65 @@
 package dataStructure;
 
 public class Items {
-    String id;
-    String name;
+    private String id;
+    private String name;
+    private Presentation presentation;
+    private Category category;
+    private SubCategory subCategory;
 
+    public Items(){
+
+    }
+
+    public Items(String id, String name, Presentation presentation, Category category, SubCategory subcategory){
+        this.id = id;
+        this.name = name;
+        this.presentation = presentation;
+        this.category = category;
+        this.subCategory = subcategory;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Presentation getPresentation() {
+        return presentation;
+    }
+
+    public void setPresentation(Presentation presentation) {
+        this.presentation = presentation;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public SubCategory getSubCategory() {
+        return subCategory;
+    }
+
+    public void setSubCategory(SubCategory subCategory) {
+        this.subCategory = subCategory;
+    }
+
+    public String toString(){
+        return this.id + " " + this.name + " " + this.presentation + " " + this.category + " " + this.subCategory;
+    }
 }
--- src/dataStructure/SubCategory.java
+++ src/dataStructure/SubCategory.java
@@ -1,21 +1,13 @@
 package dataStructure;
 
 public class SubCategory extends Category {
-    private Category category;
+
 
     public SubCategory() {
     }
 
-    public SubCategory(String id, String name, Category category, String description) {
+    public SubCategory(String id, String name,String description) {
         super(id, name, description);
-        this.category = category;
-    }
 
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
     }
 }
