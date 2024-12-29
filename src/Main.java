32e3434fc9e6b189f5173166bb1aa234eaa4247d
Cristi Lanuza <141888629+CrisGissell@users.noreply.github.com>
2024-12-28 20:31:05 -0800

    Categoria vista

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.*;
+import Presentaciones.Controller;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+
+        Controller controller = new Controller();
+
+        controller.Init();
+
     }
 }
