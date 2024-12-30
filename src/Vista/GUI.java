8fc7ea54b051b49de2c0ba7cfbe1c52f77d9c0e4
IvanCio21 <ivan.espinoza.mora@est.una.ac.cr>
2024-12-30 12:32:18 -0600

    Parte Grafica

--- src/Vista/GUI.java
+++ src/Vista/GUI.java
@@ -5,16 +5,22 @@ import javax.swing.*;
 public class GUI {
     private JTabbedPane PestaniasPanel;
     private JPanel principal;
-    private JPanel categoriaPanel;
-    private JPanel subCategoriaPanel;
-    private JPanel articulosPanel;
-    private JTextField textField1;
-    private JTextField textField2;
+    private JPanel Principal;
+    private JTable table1;
+    private JButton NEXTTTButton;
+    private JLabel codigoText;
+    private JTextField codigo;
+    private JLabel nombretext;
+    private JTextField nombre;
+    private JLabel descripcionText;
+    private JPanel search;
+    private JButton searchButton;
+    private JButton limpiarButton;
+    private JButton guardarButton;
+    private JButton eliminarButton;
     private JTextField textField3;
     private JTextField textField4;
-    private JTextField textField5;
-    private JButton buscarButton;
-    private JTable Tabla;
+    private JPanel Categoria;
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
